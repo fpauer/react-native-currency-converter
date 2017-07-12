3d451cbf06fe85e29721cb96fd723770d7728a2f
Fernando Pauer <fpauer@gmailcom>
2017-07-12 10:13:24 -0500

    Themes screen

--- app/index.js
+++ app/index.js
@@ -3,10 +3,15 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 import CurrencyList from './screens/CurrencyList';
 import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+
   $white: '#FFFFFF',
   $lightGray: '#E0E0E0',
   $border: '#E2E2E2',
@@ -15,4 +20,4 @@ EStyleSheet.build({
   // outline: 1, debug layout positions
 });
 
-export default () => <Options />;
+export default () => <Themes />;
